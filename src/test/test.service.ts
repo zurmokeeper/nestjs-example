deebe7270a6682f2e548270888a9b4d1926db5df
bruce <1658370535@qq.com>
2018-06-12 15:40:10 +0800

    first commit

---
+++ src/test/test.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TestService {
+
+  test(): string {
+    return 'test test test!';
+  }
+
+  findOneTest(): string {
+    return 'findOneTest!';
+  }
+}
